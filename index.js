e648d1575832a42124d48661d51755c967bf1a9d
Your Name <brianmwangi10808@gmail.com>
2024-01-13 19:00:50 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Define functions based on the test descriptions
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+ 
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  
+  
